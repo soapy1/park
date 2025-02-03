dfccb83a52ab67293c8a68963f8cb73db21a334b
sophia <scastellarin@quansight.com>
2025-02-03 13:46:16 -0800

    Add endpoints

--- src/land/main.py
+++ src/land/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
@@ -6,3 +6,24 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/{namespace}")
+async def get_repo(namespace: str):
+    return {"message": f"listing namespaces in {namespace}"}
+
+
+@app.get("/{namespace}/{environment}")
+async def get_repo(namespace: str, environment: str):
+    return {"message": f"listing checkpoints for {namespace}/{environment}"}
+
+
+@app.get("/{namespace}/{environment}/{checkpoint}")
+async def get_repo(namespace: str, environment: str, checkpoint: str):
+    return {"message": f"getting checkpoint {checkpoint} {namespace}/{environment}"}
+
+
+@app.post("/{namespace}/{environment}/{checkpoint}")
+async def post_repo(namespace: str, environment: str, checkpoint: str, file: UploadFile = File(...)):
+    return {"message": f"uploading checkpoint {checkpoint} {namespace}/{environment}"}
+
